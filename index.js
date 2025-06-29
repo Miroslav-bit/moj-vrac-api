b2279f57bd6fd92e6cec6ee662b95ef61ff88197
Miroslav-bit <suinfotrg@gmail.com>
2025-06-29 12:12:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,9 +61,7 @@ const prevodiOdgovora = {
   de: { da: "JA", ne: "NEIN", verovatnoca: "Wahrscheinlichkeit" },
   pt: { da: "SIM", ne: "NÃO", verovatnoca: "probabilidade" },
   it: { da: "SÌ", ne: "NO", verovatnoca: "probabilità" },
-  ru: { da: "ДА", ne: "НЕТ", verovatnoca: "вероятность" },
-  pl: { da: "TAK", ne: "NIE", verovatnoca: "prawdopodobieństwo" },
-  uk: { da: "ТАК", ne: "НІ", verovatnoca: "ймовірність" }
+  ru: { da: "ДА", ne: "НЕТ", verovatnoca: "вероятность" }
 };
 
 app.post("/api/v1/da-li-ce-se-desiti", async (req, res) => {
